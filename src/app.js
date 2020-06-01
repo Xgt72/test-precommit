7b6e3471aa8b8d870003bf003fbcc7c1a0492c7b
Xgt72 <xavier.georget@wildcodeschool.fr>
2020-06-01 14:39:39 +0200

    build path and production in app deleted

--- src/app.js
+++ src/app.js
@@ -1,7 +1,4 @@
 import express from 'express';
-import path from 'path';
-
-const isProd = process.env.NODE_ENV === 'production';
 
 const app = express();
 app.use(express.json());
@@ -15,12 +12,5 @@ app.get('/api/foo', (req, res) => {
   res.json({ foo: 'bar' });
 });
 
-if (isProd) {
-  const buildPath = path.resolve(__dirname, '../../front/build');
-  const indexHtml = path.join(buildPath, 'index.html');
-
-  app.use(express.static(buildPath));
-  app.get('*', (req, res) => res.sendFile(indexHtml));
-}
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, (err) => {
   if (err) {
-    // console.error(`ERROR: ${err.message}`);
+    console.error(`ERROR: ${err.message}`);
   } else {
-    // console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
   }
 });
